ca4d5b34cdd0f89454c6e74f88cb4db4feb6dfe3
HimaniDesai <de99hi@gmail.com>
2019-03-21 11:54:02 +0530

    first commit

--- quote/index.js
+++ quote/index.js
@@ -18,41 +18,35 @@ const path=require("path");
 
   app.post('/webhook',(req,res) =>{
     //var city="delhi";
-    var query= req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.q
-      ? req.body.result.parameters.q
-      : 'chatbot';
+    
   	var jokes=req.body.result.parameters.quotes;
   //	if(city == null)
   //		city="Delhi";
-        var w=getJoke(query);
+        var w=getJoke();
         return res.json({
           speech: w,
           displayText: w,
-          source: "wikisearch"
+          source: "quotee"
         }); 
   });
 
 var result;
-function getJoke(query)
+function getJoke()
 {
 	result=undefined;
 	const request = require('request');
 
 //let apiKey = '392e5b9bd00f4c5c35a0533f7abbac5d';
 //let city = 'portland';
-let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ query +"&format=json"
+let url = `https://random-math-quote-api.herokuapp.com/`
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    var wiki = JSON.parse(body);
-    for (var i = 0; i < 1; i++) {
-       var data = `You searched for ${wiki[1][i]}: And these are the  details — ${wiki[2][i]} Follow this link to read more — ${wiki[3][i]}` + '\n';
-        console.log(data);
-    }
-    result=data;
+    let q = JSON.parse(body);
+    let message = `It's ${q.quote}!`;
+    console.log(message);
+    result=message;
   }
 
 });
